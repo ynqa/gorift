fc3c2eb0bf0d535f7421c0f8ba351b1ee57e05f0
ynqa <un.pensiero.vano@gmail.com>
2019-11-19 20:48:41 +0900

    add resolver example

---
+++ examples/resolver/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kr/pretty"
+
+	"github.com/gorift/gorift/pkg/resolve"
+	"github.com/gorift/gorift/pkg/server"
+)
+
+func main() {
+	resolver, err := resolve.NewDefaultResolver()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	report, err := resolver.Lookup(resolve.ResolveRequest{
+		Host: server.Host("localhost"),
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%# v", pretty.Formatter(report))
+}
